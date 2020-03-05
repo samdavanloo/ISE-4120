8a08fb5ae9682380f8f90090d2b8acb23d02148b
Sam Davanloo <davanloo.1@osu.edu>
2020-03-05 00:02:31 -0500

    Update control_chart.py

--- control_chart.py
+++ control_chart.py
@@ -10,6 +10,8 @@ import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 
+
+#################### 1. X-bar and R control chart  ##########
 m=30
 n=5
 data=np.random.normal(loc=10,scale=2,size=(m,n))
@@ -45,3 +47,41 @@ plt.plot(itr,R_i,'ob-',itr,CL_R*np.ones(m),'k-',itr,LCL_R*np.ones(m),'r-',itr,UC
 plt.xlabel('Subgroup')
 plt.title('Sample range')
 plt.legend(['$R_i$','CL','LCL','UCL'],loc='upper right')
+
+
+#################### 2. X-bar and S control chart  ##########
+m=30
+n=5
+data=np.random.normal(loc=10,scale=2,size=(m,n))
+print(data)
+
+x_bar_i=np.mean(data,axis=1)
+s_i=np.std(data,axis=1,ddof=1)
+x_bar_bar=np.mean(x_bar_i)
+s_bar=np.mean(s_i)
+
+A3=1.427    # from Appendix VI
+B3=0        # from Appendix VI
+B4=2.089    # from Appendix VI
+
+CL_x=x_bar_bar
+LCL_x=x_bar_bar-A3*s_bar
+UCL_x=x_bar_bar+A3*s_bar
+
+CL_s=s_bar
+LCL_s=B3*s_bar
+UCL_s=B4*s_bar
+
+itr=list(range(m))
+
+plt.figure()
+plt.plot(itr,x_bar_i,'ob-',itr,CL_x*np.ones(m),'k-',itr,LCL_x*np.ones(m),'r-',itr,UCL_x*np.ones(m),'r-')
+plt.xlabel('Subgroup')
+plt.title('$\overline{x}$ Control Chart')
+plt.legend(['$\overline{x}_i$','CL','LCL','UCL'])
+
+plt.figure()
+plt.plot(itr,s_i,'ob-',itr,CL_s*np.ones(m),'k-',itr,LCL_s*np.ones(m),'r-',itr,UCL_s*np.ones(m),'r-')
+plt.xlabel('Subgroup')
+plt.title('$s$ Control Chart')
+plt.legend(['$s_i$','CL','LCL','UCL'],loc='upper right')
