0d43b1b64263b24593a0d0012342a1569fc9285e
Sam Davanloo <davanloo.1@osu.edu>
2020-03-26 07:37:31 -0400

    Update control_chart.py
    addition of attribute control charts

--- control_chart.py
+++ control_chart.py
@@ -6,12 +6,15 @@ Created on Mon Mar  2 23:29:08 2020
 @author: Sam
 """
 
+#################### Control Charts for Variables  ####################
+
+
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 
 
-#################### 1. X-bar and R control chart  ##########
+#################### X-bar and R control chart  ##########
 m=30
 n=5
 data=np.random.normal(loc=10,scale=2,size=(m,n))
@@ -49,7 +52,7 @@ plt.title('Sample range')
 plt.legend(['$R_i$','CL','LCL','UCL'],loc='upper right')
 
 
-#################### 2. X-bar and S control chart  ##########
+#################### X-bar and S control chart  ##########
 m=30
 n=5
 data=np.random.normal(loc=10,scale=2,size=(m,n))
@@ -85,3 +88,118 @@ plt.plot(itr,s_i,'ob-',itr,CL_s*np.ones(m),'k-',itr,LCL_s*np.ones(m),'r-',itr,UC
 plt.xlabel('Subgroup')
 plt.title('$s$ Control Chart')
 plt.legend(['$s_i$','CL','LCL','UCL'],loc='upper right')
+
+
+
+#################### Control Charts for Attributes  ####################
+
+#################### p control chart  ##########
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(8) 
+
+m=30   # number of samples
+n=50   # sample size
+D=np.random.randint(19,size=m) # number of nonconformings
+print(D)
+
+p_bar=np.sum(D)/(m*n)
+print(p_bar)
+
+p=D/n
+CL=p_bar
+UCL=p_bar+3*np.sqrt(p_bar*(1-p_bar)/n)
+LCL=p_bar-3*np.sqrt(p_bar*(1-p_bar)/n)
+
+
+itr=list(range(m))
+
+plt.figure()
+plt.plot(itr,p,'ob-',itr,CL*np.ones(m),'k-',itr,LCL*np.ones(m),'r-',itr,UCL*np.ones(m),'r-')
+plt.xlabel('Sample')
+plt.title('p Control Chart')
+plt.legend(['$p$','CL','LCL','UCL'])
+
+
+#################### np control chart  ##########
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(8) 
+
+m=30   # number of samples
+n=50   # sample size
+D=np.random.randint(19,size=m) # number of nonconformings
+print(D)
+
+p_bar=np.sum(D)/(m*n)
+print(p_bar)
+
+CL=n*p_bar
+UCL=n*p_bar+3*np.sqrt(n*p_bar*(1-p_bar))
+LCL=n*p_bar-3*np.sqrt(n*p_bar*(1-p_bar))
+
+itr=list(range(m))
+
+plt.figure()
+plt.plot(itr,D,'ob-',itr,CL*np.ones(m),'k-',itr,LCL*np.ones(m),'r-',itr,UCL*np.ones(m),'r-')
+plt.xlabel('Sample')
+plt.title('np Control Chart')
+plt.legend(['D','CL','LCL','UCL'])
+
+
+#################### c control chart  ##########
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(8) 
+
+m=30   # number of samples (note that n=1 here)
+num_defects=np.random.poisson(lam=22,size=m) # lambda is the parameter of poisson distribution 
+print(num_defects)
+
+c_bar=np.sum(num_defects)/m
+print(c_bar)
+
+CL=c_bar
+UCL=c_bar+3*np.sqrt(c_bar)
+LCL=c_bar-3*np.sqrt(c_bar)
+
+itr=list(range(m))
+
+plt.figure()
+plt.plot(itr,num_defects,'ob-',itr,CL*np.ones(m),'k-',itr,LCL*np.ones(m),'r-',itr,UCL*np.ones(m),'r-')
+plt.xlabel('Sample')
+plt.title('c Control Chart')
+plt.legend(['N0. of defects','CL','LCL','UCL'])
+
+
+
+#################### u control chart  ##########
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(8) 
+
+m=30   # number of samples
+n=50
+total_num_defects=np.random.poisson(lam=5,size=m) # total number of defects in n items in a sample
+print(total_num_defects)
+u=total_num_defects/n
+print(u)
+
+u_bar=np.sum(total_num_defects)/(m*n)
+print(u_bar)
+
+CL=u_bar
+UCL=u_bar+3*np.sqrt(u_bar/n)
+LCL=u_bar-3*np.sqrt(u_bar/n)
+
+itr=list(range(m))
+
+plt.figure()
+plt.plot(itr,u,'ob-',itr,CL*np.ones(m),'k-',itr,LCL*np.ones(m),'r-',itr,UCL*np.ones(m),'r-')
+plt.xlabel('Sample')
+plt.title('u Control Chart')
+plt.legend(['average N0. of defects','CL','LCL','UCL'])
